77e8ac00c9b84919819df20ec34f17dc9bf92762
raosan <raosan@hyperjump.tech>
2020-11-05 10:37:28 +0700

    add notification function

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,20 @@
+importScripts('https://www.gstatic.com/firebasejs/8.0.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.0.0/firebase-messaging.js');
+importScripts("./firebase-config.js");
+
+firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: 'icons/icon-192x192.png'
+  };
+
+  self.registration.showNotification(notificationTitle,
+    notificationOptions);
+});
