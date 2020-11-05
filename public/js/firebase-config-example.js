cf3c2d042f530408267941010d4cd088d7175124
raosan <raosan@hyperjump.tech>
2020-11-05 15:39:37 +0700

    restructure files

---
+++ public/js/firebase-config-example.js
@@ -0,0 +1,12 @@
+// rename this file to: firebase-config.js
+// then fill the values with your firebase config
+
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
